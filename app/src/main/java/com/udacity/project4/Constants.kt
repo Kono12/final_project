da9e28817312618cc015a90cd717d9958573f8a8
KONO <aleslam12345@gmail.com>
2022-09-24 02:58:16 +0200

    error fix

---
+++ app/src/main/java/com/udacity/project4/Constants.kt
@@ -0,0 +1,6 @@
+package com.udacity.project4
+
+object Constants {
+    const val SIGN_IN_REQUEST_CODE = 1001
+    val TAG="Mohamed"
+}
--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,8 +1,20 @@
 package com.udacity.project4.authentication
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import com.udacity.project4.Constants
+import com.udacity.project4.Constants.SIGN_IN_REQUEST_CODE
 import com.udacity.project4.R
+import com.udacity.project4.locationreminders.RemindersActivity
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -16,11 +28,44 @@ class AuthenticationActivity : AppCompatActivity() {
         setContentView(R.layout.activity_authentication)
 //         TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
 
-// bla
+  startActivity(Intent(this, RemindersActivity::class.java))
+//                finish()
 //          TODO: If the user was authenticated, send him to RemindersActivity
+//          use shared
+
 
 //          TODO: a bonus is to customize the sign in flow to look nice using :
         //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
 
+
+    }
+
+    fun LOGIN_CLICKED(view: View) {
+
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        //starting sign in and login flow
+        startActivityForResult(
+            AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers)
+                .build(), SIGN_IN_REQUEST_CODE
+        )
+    }
+
+    //login flow result
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == SIGN_IN_REQUEST_CODE) {
+
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                //navigate to the app
+                startActivity(Intent(this, RemindersActivity::class.java))
+                finish()
+            }
+        }
     }
 }
